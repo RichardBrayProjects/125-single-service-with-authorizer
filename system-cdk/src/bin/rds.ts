2f02a260a0cea37b7779b930f0cf367dd04c4bf0
RichardBrayProjects <user@example.com>
2026-01-16 08:17:01 +0000

    new aurora RDS database

--- system-cdk/src/bin/rds.ts
+++ system-cdk/src/bin/rds.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { RdsStack } from '../lib/rdsStack';
+import * as cdk from "aws-cdk-lib";
+import { RdsStack } from "../lib/rdsAuroraStack";
 
 const app = new cdk.App();
 
 // Use default AWS account and region from AWS CLI configuration
 // No explicit env specification - CDK will use default credentials
-new RdsStack(app, 'system-rds');
+new RdsStack(app, "system-rds");
