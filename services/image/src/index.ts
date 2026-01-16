1bd2c426f0637621c2170416f5fcbcf1016ed341
RichardBrayProjects <user@example.com>
2026-01-16 07:04:04 +0000

    typing for serverlessExpress

--- services/image/src/index.ts
+++ services/image/src/index.ts
@@ -1,7 +1,8 @@
 import serverlessExpress from "@vendia/serverless-express";
+import type { Handler } from "aws-lambda";
 import { app } from "./app";
 
 console.log("BOOT: image service index.ts loaded");
 
 // Normal vendia handler (no wrapper, no globals, no ALS in your code)
-export const handler = serverlessExpress({ app });
+export const handler: Handler = serverlessExpress({ app });
